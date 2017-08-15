ab88f8a2da1bf0c5bf8909bd0173fae1c3d7d65f
yemingjun <m17710627071@163.com>
2017-08-15 16:11:26 +0800

    .

---
+++ Demo/3D分页翻转滑块/js/index.js
@@ -0,0 +1,45 @@
+$(function(){
+	itemShow 		= 8;
+	indexedItemShow = itemShow - 1;
+	itemList 		= $('.portfolio-item');
+	itemWrapper  	= $('.portfolio');
+	rotation      	= ['flipped-vertical-bottom', 'flipped-vertical-top', 'flipped-horizontal-left', 'flipped-horizontal-right'];
+	navigation		= $(".navigation a");
+	
+	for (var i = 0; i < itemShow; i++) {
+		itemImage	= itemList.children('li:eq(' + i + ')').children('img');
+		itemSrc 		= itemImage.attr('src');
+		itemAlt		= itemImage.attr('alt');
+		item			= '<div style="background:url(' + itemSrc + ')"> <span>' + itemAlt + '</span></div>'
+		itemWrapper.append(item);
+	}
+	
+	navigation.on('click', function(event){
+		event.preventDefault();
+		navigation.removeClass('selected');
+		$(this).addClass('selected');
+		page= $(this).attr('data-page');
+		for (var i = 0; i <= indexedItemShow; i ++) {
+			random 		= Math.floor(Math.random() * 4);
+			animation 	= rotation[random];
+			item 		= itemWrapper.children('div:eq(' + i +')');
+			item.addClass('animated ' + animation);
+			window.setTimeout(function(index){
+				return 	function() {
+					indexReal	= (page == 1) ? index : (index + (page - 1));
+					indexHost 	= indexReal + (indexedItemShow * (page - 1));
+					itemImage	= itemList.children('li:eq(' + indexHost + ')').children('img');
+					itemSrc 		= itemImage.attr('src');
+					itemAlt		= itemImage.attr('alt');
+					itemCurrent 	= itemWrapper.children('div:eq(' + index + ')');
+					
+           			itemCurrent.css('background', 'url(' + itemSrc + ')');
+            			itemCurrent.children('span').text(itemAlt);
+				};
+			} (i), 500);
+			item.on('transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd', function() { 
+        			$(this).removeClass();
+      		});
+		}
+	});
+});
